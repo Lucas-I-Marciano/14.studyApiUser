f4e8766ea908a01c3cbf2826c46390a1bcdb5507
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-07 19:14:08 -0300

    :sparkles: feat: Adding 'usuarios' routes to index

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser");
 
 const produtoRouter = require("./produtoRoute");
+const usuariosRouter = require("./usuariosRouter.js");
 
 module.exports = (app) => {
   //prettier-ignore
   app
     .use(bodyParser.json())
-    .use("/produto", produtoRouter);
+    .use("/produtos", produtoRouter)
+    .use("/usuarios", usuariosRouter)
 };
