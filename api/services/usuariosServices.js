06b898d87c76527a9a2ba3c07730aa84c6140e86
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-08 22:07:33 -0300

    :sparkles: feat: CRUD: Create - 'usuario' Service

---
+++ api/services/usuariosServices.js
@@ -0,0 +1,24 @@
+const db = require("../database/models/index.js");
+const createHashPassword = require("../utils/createHashPass.js");
+const uuid = require("uuid");
+
+const userDb = db.usuarios;
+
+class UsuarioService {
+  async createUser(dto) {
+    const { hashPass, randomB } = createHashPassword(dto["senha"]);
+    return await userDb.create({
+      id: uuid.v4(),
+      nome: dto["nome"],
+      email: dto["email"],
+      senha: hashPass,
+      salt: randomB,
+    });
+  }
+}
+
+const a = new UsuarioService();
+
+a.createUser({ nome: "Lucas", email: "l@l", senha: "asd" });
+
+module.exports = UsuarioService;
