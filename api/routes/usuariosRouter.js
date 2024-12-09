01154e2c97b50ebd723086a8c363de94e619797b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-09 18:14:36 -0300

    :books: docs: Adding function on routes

--- api/routes/usuariosRouter.js
+++ api/routes/usuariosRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
+const UsuarioController = require("../controllers/usuariosController.js");
 
 const usuariosRouter = Router();
+const usuarioController = new UsuarioController();
 
 usuariosRouter
   .get("/")
   .get("/id/:id")
-  .post("/")
+  .post("/", (req, res) => usuarioController.createUser(req, res))
   .put("/id/:id")
   .delete("/id/:id");
 
