061bfe881c82885c888d380f25b437718d5a329a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-12 17:32:05 -0300

    :sparkles: feat: 'roles' Router

--- api/routes/rolesRoute.js
+++ api/routes/rolesRoute.js
@@ -5,6 +5,11 @@ const rolesController = new RolesController();
 
 const rolesRouter = Router();
 
-rolesRouter.post("/", (req, res) => rolesController.createRole(req, res));
+rolesRouter
+  .post("/", (req, res) => rolesController.createRole(req, res))
+  .get("/", (req, res) => rolesController.getAllRoles(req, res))
+  .get("/:id", (req, res) => rolesController.getRoleById(req, res))
+  .put("/:id", (req, res) => rolesController.updateRoleById(req, res))
+  .delete("/:id", (req, res) => rolesController.deleteRoleById(req, res));
 
 module.exports = rolesRouter;
