291b239ece978cccc7c10b1382af24bcbe4572ba
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-12 17:58:23 -0300

    :sparkles: feat: Setting 'usuario-roles' Association up

--- api/database/models/usuarios.js
+++ api/database/models/usuarios.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      usuarios.belongsToMany(models["roles"], {
+        through: "usuarios_roles",
+      });
     }
   }
   usuarios.init(
--- api/database/models/usuarios_roles.js
+++ api/database/models/usuarios_roles.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class usuarios_roles extends Model {
     /**
@@ -13,12 +11,27 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  usuarios_roles.init({
-    usuario_id: DataTypes.UUID,
-    role_id: DataTypes.UUID
-  }, {
-    sequelize,
-    modelName: 'usuarios_roles',
-  });
+  usuarios_roles.init(
+    {
+      usuario_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: "usuarios",
+          key: "id",
+        },
+      },
+      role_id: {
+        type: DataTypes.UUID,
+        references: {
+          model: "roles",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "usuarios_roles",
+    }
+  );
   return usuarios_roles;
 };
