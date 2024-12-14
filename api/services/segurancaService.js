d83fbfd222360605ef11ad16e77fa557dd329c61
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-13 21:50:42 -0300

    :sparkles: feat: Creating Service to implement ACL(Access Control List)
    Expected a object with two arrays: 'roles' and 'permissoes'

---
+++ api/services/segurancaService.js
@@ -0,0 +1,59 @@
+const { Op } = require("sequelize");
+const db = require("../database/models/index.js");
+
+const usuarioModel = db["usuarios"];
+const rolesModel = db["roles"];
+const permissoesModel = db["permissoes"];
+
+class SegurancaService {
+  async cadastrarAcl(dto) {
+    const roles = await rolesModel.findAll({
+      where: {
+        id: { [Op.in]: dto["roles"] },
+      },
+    });
+    const permissoes = await permissoesModel.findAll({
+      where: {
+        id: { [Op.in]: dto["permissoes"] },
+      },
+    });
+
+    const usuario = await usuarioModel.findOne({
+      where: {
+        id: dto["usuarioId"],
+      },
+    });
+
+    await usuario.setUsuarios_e_roles(roles);
+    await usuario.setUsuarios_e_permissoes(permissoes);
+
+    // To see all params allowed on findOne/findAll method --> //https://sequelize.org/api/v6/class/src/model.js~model#static-method-findAll
+    const usuarioAtualizado = usuarioModel.findOne({
+      where: {
+        id: dto["usuarioId"],
+      },
+      include: [
+        {
+          model: rolesModel,
+          as: "usuarios_e_roles",
+          attributes: ["id", "nome"],
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: permissoesModel,
+          as: "usuarios_e_permissoes",
+          attributes: ["id", "nome"],
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+    });
+
+    return usuarioAtualizado;
+  }
+}
+
+module.exports = SegurancaService;
