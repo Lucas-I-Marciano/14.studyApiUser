df53dc9a072d23a8f0e0007a5f5540ca47e2d6d3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-17 18:01:35 -0300

    :sparkles: feat: Implementing permissoes middleware on 'seguranca' Route

--- api/routes/segurancaRoute.js
+++ api/routes/segurancaRoute.js
@@ -1,19 +1,24 @@
 const { Router } = require("express");
 const SegurancaController = require("../controllers/segurancaController.js");
 const rolesMiddleware = require("../middlewares/rolesMiddleware.js");
+const permissoesMiddleware = require("../middlewares/permissoesMiddleware.js");
 
 const segurancaRouter = Router();
 const segurancaController = new SegurancaController();
 
 segurancaRouter
-  .get("/acl", (req, res) =>
+  .get("/acl", permissoesMiddleware(["ler"]), (req, res) =>
     segurancaController.listaPermissoesERoles(req, res)
   )
-  .post("/acl", rolesMiddleware(["admin"]), async (req, res) =>
-    segurancaController.cadastrarAcl(req, res)
+  .post(
+    "/acl",
+    permissoesMiddleware(["criar"]),
+    rolesMiddleware(["admin"]),
+    async (req, res) => segurancaController.cadastrarAcl(req, res)
   )
   .post(
     "/roles/cadastro-permissoes",
+    permissoesMiddleware(["criar"]),
     rolesMiddleware(["admin"]),
     async (req, res) => segurancaController.permissoesDasRoles(req, res)
   );
