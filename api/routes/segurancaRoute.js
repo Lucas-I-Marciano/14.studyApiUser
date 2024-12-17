88c8e92043a1b6cd4825f605360d7944c4a7465d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-17 17:53:41 -0300

    :sparkles: feat: Implementing roles middleware on 'seguranca' Route

--- api/routes/segurancaRoute.js
+++ api/routes/segurancaRoute.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const SegurancaController = require("../controllers/segurancaController.js");
+const rolesMiddleware = require("../middlewares/rolesMiddleware.js");
 
 const segurancaRouter = Router();
 const segurancaController = new SegurancaController();
@@ -8,9 +9,13 @@ segurancaRouter
   .get("/acl", (req, res) =>
     segurancaController.listaPermissoesERoles(req, res)
   )
-  .post("/acl", async (req, res) => segurancaController.cadastrarAcl(req, res))
-  .post("/roles/cadastro-permissoes", async (req, res) =>
-    segurancaController.permissoesDasRoles(req, res)
+  .post("/acl", rolesMiddleware(["admin"]), async (req, res) =>
+    segurancaController.cadastrarAcl(req, res)
+  )
+  .post(
+    "/roles/cadastro-permissoes",
+    rolesMiddleware(["admin"]),
+    async (req, res) => segurancaController.permissoesDasRoles(req, res)
   );
 
 module.exports = segurancaRouter;
